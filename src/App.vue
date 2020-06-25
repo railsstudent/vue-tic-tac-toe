3a00602ebf8b8e1f7513a6149dc0f2cb3076111d
Connie Leung <cleung334@gmail.com>
2020-06-25 20:38:07 +0800

    feat: Game component
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Game />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import Game from "./components/Game.vue";
 
 @Component({
   components: {
-    HelloWorld
+    Game
   }
 })
 export default class App extends Vue {}
---
+++ src/components/Game.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1>Vue Tic Tac Toe</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Game extends Vue {
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h1 {
+  color: goldenrod;
+  font-size: 2rem;
+  font-weight: 600;
+}
+</style>
